cf6348e45679e6e432fac27773ed978d1c18340d
jxp1147 <2797530119@qq.com>
2024-04-18 00:19:26 +0800

    first commit

---
+++ src/test/java/org/example/doex/DoexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
